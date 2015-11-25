1064b9e8ddd3f0d6da577ab7f447087e8f144b72
yarostbaklajana <yarostbaklajana@gmail.com>
2015-11-25 05:14:59 +0200

    added EditContactController

---
+++ src/main/java/servlets/EditContactController.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by yaros on 25.11.2015.
+ */
+@WebServlet(name = "EditContactController")
+public class EditContactController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
