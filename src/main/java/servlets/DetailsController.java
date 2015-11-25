cf2f41d5ece3e4b6862e5e4ef66acf9506cca908
yarostbaklajana <yarostbaklajana@gmail.com>
2015-11-25 04:50:07 +0200

    added functionalyty to DetailsController

--- src/main/java/servlets/DetailsController.java
+++ src/main/java/servlets/DetailsController.java
@@ -1,12 +1,15 @@
 package servlets;
 
+import dao.PhoneBookDAO;
+import exceptions.DAOException;
+import models.Contact;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Created by yaros on 24.11.2015.
@@ -14,11 +17,23 @@ import java.io.PrintWriter;
 @WebServlet("/PhoneBook/details")
 public class DetailsController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter out = response.getWriter();
-        out.print("Hello from details!");
+
+
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PhoneBookDAO dao = new PhoneBookDAO();
+        String id = request.getParameter("id");
+        try {
+
+            Contact contact = dao.getContact(Integer.valueOf(id));
+            request.setAttribute("contact", contact);
+
+        } catch (DAOException e) {
+            e.printStackTrace();
+        }
+
         request.getRequestDispatcher("WEB-INF/JSPs/details.jsp").forward(request, response);
     }
 }
