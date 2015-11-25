2b893faf9bcbc85506e92ae92ea1009c97eab4e9
yarostbaklajana <yarostbaklajana@gmail.com>
2015-11-25 05:13:34 +0200

    deleted setAttribute for ID

--- src/main/java/servlets/DetailsController.java
+++ src/main/java/servlets/DetailsController.java
@@ -19,21 +19,17 @@ public class DetailsController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 
-
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PhoneBookDAO dao = new PhoneBookDAO();
         String id = request.getParameter("id");
         try {
-
             Contact contact = dao.getContact(Integer.valueOf(id));
             request.setAttribute("contact", contact);
-
         } catch (DAOException e) {
             e.printStackTrace();
         }
-
         request.getRequestDispatcher("WEB-INF/JSPs/details.jsp").forward(request, response);
     }
 }
