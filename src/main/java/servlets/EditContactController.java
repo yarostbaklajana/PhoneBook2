b3e9e9f3655705f4c4b90424a1391058f27b6756
yarostbaklajana <yarostbaklajana@gmail.com>
2015-11-27 11:19:09 +0200

    added functionality to EditController

--- src/main/java/servlets/EditContactController.java
+++ src/main/java/servlets/EditContactController.java
@@ -1,5 +1,9 @@
 package servlets;
 
+import dao.PhoneBookDAO;
+import exceptions.DAOException;
+import models.Contact;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,13 +14,37 @@ import java.io.IOException;
 /**
  * Created by yaros on 25.11.2015.
  */
-@WebServlet(name = "EditContactController")
+@WebServlet("/PhoneBook/edit")
 public class EditContactController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        int id = Integer.parseInt(request.getParameter("id"));
+        PhoneBookDAO dao = new PhoneBookDAO();
 
+        try {
+            Contact contact = new Contact(id, firstName, lastName);
+            dao.updateContact(contact);
+
+        } catch (DAOException e) {
+            e.printStackTrace();
+        }
+        response.sendRedirect("/details?id=" + id);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter("id");
+        PhoneBookDAO dao = new PhoneBookDAO();
+        try {
+            Contact contact = dao.getContact(Integer.valueOf(id));
+            request.setAttribute("contact", contact);
+            renderEditPage(request, response);
+        } catch (DAOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void renderEditPage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/JSPs/edit.jsp").forward(request, response);
     }
 }
