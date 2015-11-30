b5a3af6c6c42d1ca651d8a4c5a2a3696349afd61
yarostbaklajana <yarostbaklajana@gmail.com>
2015-11-30 22:43:11 +0200

    added class ContactValidator

---
+++ src/main/java/validation/ContactValidator.java
@@ -0,0 +1,29 @@
+package validation;
+
+import models.Contact;
+
+/**
+ * Created by yaros on 30.11.2015.
+ */
+public class ContactValidator {
+    public ValidationResult validate(Contact contact) {
+        ValidationResult validationResult = new ValidationResult();
+        if(isEmptyOrNull(contact.getFirstName())) {
+            validationResult.addError("First Name is empty");
+        }
+
+        if(isEmptyOrNull(contact.getLastName())) {
+            validationResult.addError("Last Name is empty");
+        }
+
+        if(validationResult.getErrors().size() == 0) {
+            validationResult.setIsValid(true);
+        }
+
+        return validationResult;
+    }
+
+    private boolean isEmptyOrNull(String field) {
+        return field.equals("") || field.equals(null);
+    }
+}
