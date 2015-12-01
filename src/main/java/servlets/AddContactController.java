ddc9334785d10a09300d85a40a7adf9d690538c3
yarostbaklajana <yarostbaklajana@gmail.com>
2015-12-01 10:18:58 +0200

    changed validationn implementation

--- src/main/java/servlets/AddContactController.java
+++ src/main/java/servlets/AddContactController.java
@@ -3,6 +3,8 @@ package servlets;
 import dao.PhoneBookDAO;
 import exceptions.DAOException;
 import models.Contact;
+import validation.ContactValidator;
+import validation.ValidationResult;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -28,35 +30,28 @@ public class AddContactController extends HttpServlet {
             throws ServletException, IOException {
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
+        Contact contact = new Contact(firstName, lastName);
+        ContactValidator validator = new ContactValidator();
+        ValidationResult result = validator.validate(contact);
 
-        request.setAttribute("firstName", firstName);
-        request.setAttribute("lastName", lastName);
-
-        List<String> errorMessages = new ArrayList<String>();
-
-        if (firstName.equals("")) {
-            errorMessages.add("The first name field is empty!");
-        }
-
-        if (lastName.equals("")) {
-            errorMessages.add("The last name field is empty!");
-        }
-
-        if (!errorMessages.isEmpty()) {
-            request.setAttribute("errorMessages", errorMessages);
+        if (result.getIsValid() == false) {
+            request.setAttribute("errorMessages", result.getErrors());
+            request.setAttribute("contact", contact);
             renderAddPage(request, response);
             return;
         }
 
         try {
             PhoneBookDAO dao = new PhoneBookDAO();
-            dao.addContact(new Contact(firstName, lastName));
+            dao.addContact(contact);
+            response.sendRedirect("/phonebook");
         } catch (DAOException e) {
+            List<String> errorMessages = new ArrayList<String>();
             errorMessages.add(e.getMessage());
             request.setAttribute("errorMessages", errorMessages);
             renderAddPage(request, response);
         }
-        response.sendRedirect("/phonebook");
+
     }
 
     private void renderAddPage(HttpServletRequest request, HttpServletResponse response)
--- src/main/webapp/WEB-INF/JSPs/addpage.jsp
+++ src/main/webapp/WEB-INF/JSPs/addpage.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-         pageEncoding="UTF-8"%>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+         pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -12,8 +12,9 @@
 
 
 <form action="/add" method="post">
-    <input type="text" name="firstName" value="<%= request.getAttribute("firstName") %>" placeholder="First Name"><br>
-    <input type="text" name="lastName" value="<%= request.getAttribute("lastName") %>" placeholder="Last Name"><br>
+    <input type="text" name="firstName" value="${contact.firstName}" placeholder="First Name"><br>
+    <input type="text" name="lastName" value="${contact.lastName}" placeholder="Last Name"><br>
+
     <p>
         <a href="/phonebook">Cancel</a>
         <input type="submit" value="Save">
@@ -23,12 +24,12 @@
 
 <p>
     <c:if test="${errorMessages ne null}">
-        <ul>
-            <c:forEach items="${errorMessages}" var="errorMessage">
-                <li>${errorMessage}</li>
-            </c:forEach>
-       </ul>
-    </c:if>
+<ul>
+    <c:forEach items="${errorMessages}" var="errorMessage">
+        <li>${errorMessage}</li>
+    </c:forEach>
+</ul>
+</c:if>
 </p>
 
 </body>
