aedb3948eea25afc0645c3d246136f3a2e2cc1d0
yarostbaklajana <yarostbaklajana@gmail.com>
2015-12-11 14:43:52 +0200

    Created deletPhoneController

---
+++ src/main/java/servlets/DeletePhoneController.java
@@ -0,0 +1,32 @@
+package servlets;
+
+import dao.PhoneBookDAO;
+import exceptions.DAOException;
+import models.ContactDetails;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeletePhoneController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String phoneIdString = request.getParameter("phoneId");
+        int id = Integer.parseInt(phoneIdString);
+        int contactId = Integer.parseInt(request.getParameter("contactId"));
+        try {
+            PhoneBookDAO dao = new PhoneBookDAO();
+            dao.deletePhoneNumber(id);
+            response.sendRedirect("/details?id=" + contactId);
+        } catch (DAOException e) {
+            List<String> errorMessages = new ArrayList<String>();
+            errorMessages.add(e.getMessage());
+            request.setAttribute("errorMessages", errorMessages);
+            request.getRequestDispatcher("WEB-INF/JSPs/details.jsp").forward(request, response);
+        }
+    }
+}
